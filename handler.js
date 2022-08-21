3867ac2c91b4dea3a7c841c3b8a0fbae2d44b1a2
Eduardo Rodrigues <silva.rodriguesedu@icloud.com>
2022-08-21 12:49:50 -0300

    :construction: Configuração no arquivo handler para utilização do Dynamo local
    Configuração para verificar se a aplicação está executando no modo offline, executar o DynamoDB local.

--- handler.js
+++ handler.js
@@ -8,6 +8,21 @@ const pacientes = [
 const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require("uuid");
 
+const dynamodbOfflineOptions = {
+    region: "localhost",
+    endpoint: "http://localhost:8000"
+}
+
+const isOffline = () => process.env.IS_OFFLINE;
+
+const dynamoDb = isOffline()
+    ? new AWS.DynamoDB.DocumentClient(dynamodbOfflineOptions)
+    : new AWS.DynamoDB.DocumentClient();
+
+const params = {
+    TableName: process.env.PACIENTES_TABLE,
+};
+
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const params = {
     TableName: "PACIENTES",

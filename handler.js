b742cc0e9f4d4c10a1247870fe53b064630173cf
Eduardo Rodrigues <silva.rodriguesedu@icloud.com>
2022-08-20 21:44:29 -0300

    :construction: Implementation Databse DynamoDB

--- handler.js
+++ handler.js
@@ -1,23 +1,35 @@
 'use strict';
-
 const pacientes = [
     {id: 1, nome: "Eduardo", dataNascimento: '1982-08-28'},
     {id: 2, nome: "Dayane", dataNascimento: '2005-08-30'},
     {id: 3, nome: "Neuza", dataNascimento: '1967-09-11'}
 ]
 
+const AWS = require('aws-sdk')
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+const params = {
+    TableName: 'PACIENTES'
+};
+
 module.exports.listarPacientes = async (event) => {
-    console.log(event)
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-          pacientes
-      },
-      null,
-      2
-    ),
-  };
+    try {
+        let data = await dynamoDb.scan(params).promise();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data.Items),
+        };
+    } catch (err) {
+        console.log("Error", err);
+        return {
+            statusCode: err.statusCode ? err.statusCode : 500,
+            body: JSON.stringify({
+                error: err.name ? err.name : "Exception",
+                message: err.message ? err.message : "Unknown error",
+            }),
+        };
+    }
 };
 
 module.exports.obterPaciente = async (event) => {
